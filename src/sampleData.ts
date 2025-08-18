1112c93e075158d3fa16cc99446f886b7cf64945
qiongwang-oai <qiongwang@openai.com>
2025-08-18 10:03:28 -0700

    Set Server_subsystem.json as default sampleProject

--- src/sampleData.ts
+++ src/sampleData.ts
@@ -1,4 +1,4 @@
 import { Project } from './models'
-import sample from './assets/Sample_system.json'
+import sample from './assets/Server_subsystem.json'
 
 export const sampleProject: Project = sample as any
